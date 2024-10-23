70693840c14ada0f829fba359a14647b1ad58812
IvyedSG <deyvioscarzanches@gmail.com>
2024-10-23 11:56:51 -0500

    fusion con felipe

--- components/layout/user-nav.tsx
+++ components/layout/user-nav.tsx
@@ -38,6 +38,7 @@ export function UserNav() {
                 {session.user?.email}
               </p>
             </div>
+            
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
